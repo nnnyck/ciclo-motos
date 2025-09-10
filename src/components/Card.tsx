1e504915a7b89611ad793cb528a4b0f915d46d98
Nyckolas <nyckolas.seixas@penseapp.com.br>
2025-09-10 20:06:53 -0300

    fix: problema de deploy

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { SVGProps } from "react";
 
 interface CardProps {
   title: string;
   description: string;
-  icon?: React.ReactNode;
+  icon?: React.ReactElement<SVGProps<SVGSVGElement>>; // <- aqui
   iconBgClass?: string;
   iconColorClass?: string;
 }
@@ -12,26 +12,23 @@ export default function Card({
   title,
   description,
   icon,
-  iconBgClass = "bg-error-light",
-  iconColorClass = "text-error",
+  iconBgClass = "bg-red-400",
+  iconColorClass = "text-red-600",
 }: CardProps) {
   return (
     <div className="text-center p-6 bg-white rounded-lg shadow-md">
-  {icon && (
-    <div
-      className={`w-16 h-16 ${iconBgClass} rounded-full flex items-center justify-center mx-auto mb-4 relative z-10`}
-    >
-      {/* Círculo menor / ícone */}
-      {React.cloneElement(icon as React.ReactElement, {
-        className: `w-8 h-8 text-red-600 absolute inset-0 m-auto z-0`,
-      })}
+      {icon && (
+        <div
+          className={`w-16 h-16 ${iconBgClass} rounded-full flex items-center justify-center mx-auto mb-4 relative`}
+        >
+          {/* Aplica a classe direto no SVG */}
+          {React.cloneElement(icon, {
+            className: `w-8 h-8 ${iconColorClass} absolute inset-0 m-auto z-0`,
+          })}
+        </div>
+      )}
+      <h3 className="text-lg font-montserrat font-bold text-gray-800 mb-2">{title}</h3>
+      <p className="text-gray-500 text-sm">{description}</p>
     </div>
-  )}
-  <h3 className="text-lg font-montserrat font-bold text-gray-800 mb-2">
-    {title}
-  </h3>
-  <p className="text-gray-500 text-sm">{description}</p>
-</div>
-
   );
 }
